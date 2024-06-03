ef641add6d5a9579bbda750082c7be5e40a3e7ec
ArunKashyap <arunbhagat132@gmail.com>
2024-06-03 15:32:37 +0530

    01

---
+++ 01.js
@@ -0,0 +1,37 @@
+
+// basics of JavaScript
+var userName = "Arun"
+let userSecondName = "Kashyap"
+const dateOfBirth  = 2002;
+console.log(userName);
+console.log(userSecondName);
+console.log(dateOfBirth);
+console.table([userName,userSecondName,dateOfBirth])
+console.log(typeof(dateOfBirth));
+
+
+
+"use strict"; // treat all the js code as newer version
+
+// alert("I am alerting every one with a popup on the browser but this is not node js not the browser so it will not work")
+
+// DATA TYPES
+
+console.log("DATATYPES");
+
+let temp = null // null is a standalone value which means nothing 
+let name; // this means the variable name is undedefined 
+
+
+// console.log(temp);
+
+
+//datatype conversions
+console.log("DATATYPES CONVERSIONS");
+
+
+let obj = null
+console.log(typeof obj);
+
+
+  
